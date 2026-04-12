dddda70bc62489a42b5378fee335b67b781e487a
Prakash Deep Sharma <prakashdeep2902@gmail.com>
2026-04-12 14:55:54 +0530

    fexd

--- javascript/day05/AsynchronousJavaScript.js
+++ javascript/day05/AsynchronousJavaScript.js
@@ -1,221 +1,221 @@
-// // // then and catch concept
+// then and catch concept
 
-// // fetch("https://jsonplaceholder.typicode.com/users")
-// //   .then((result) => {
-// //     return result.json();
-// //   })
-// //   .then((data) => {
-// //     arr = data;
-// //     // console.log(data);
-// //     return data;
-// //   })
-// //   .catch((err) => {
-// //     return err;
-// //   });
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    arr = data;
+    // console.log(data);
+    return data;
+  })
+  .catch((err) => {
+    return err;
+  });
 
-// // // const name = "prakash";
+// const name = "prakash";
 
-// // // promish exmaple of fetching apis
-// // function getUsers() {
-// //   return new Promise((resolve, reject) => {
-// //     fetch("https://jsonplaceholder.typicode.com/users")
-// //       .then((res) => res.json())
-// //       .then(resolve)
-// //       .catch(reject);
-// //   });
-// // }
+// promish exmaple of fetching apis
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then(resolve)
+      .catch(reject);
+  });
+}
 
-// // const users = getUsers()
-// //   .then((data) => {
-// //     // console.log(data);
-// //   })
-// //   .catch((err) => err);
+const users = getUsers()
+  .then((data) => {
+    // console.log(data);
+  })
+  .catch((err) => err);
 
-// // async function CallUserByAsyncAwait() {
-// //   try {
-// //     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-// //     if (!res.ok) {
-// //       throw new error("Failed to fetch users");
-// //     }
-// //     const data = await res.json();
-// //     const Arr = Object.fromEntries(
-// //       Object.entries(data).map(([key, value]) => {
-// //         return [key, value];
-// //       }),
-// //     );
-// //     console.log(Arr);
-// //   } catch (error) {
-// //     console.error(error);
-// //   }
-// // }
+async function CallUserByAsyncAwait() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!res.ok) {
+      throw new error("Failed to fetch users");
+    }
+    const data = await res.json();
+    const Arr = Object.fromEntries(
+      Object.entries(data).map(([key, value]) => {
+        return [key, value];
+      }),
+    );
+    console.log(Arr);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// // CallUserByAsyncAwait();
+CallUserByAsyncAwait();
 
-// // Promise.resolve()
-// //   .then(() => {
-// //     console.log("1");
-// //   })
-// //   .then(() => {
-// //     console.log("2");
-// //   });
+Promise.resolve()
+  .then(() => {
+    console.log("1");
+  })
+  .then(() => {
+    console.log("2");
+  });
 
-// // Promise.resolve()
-// //   .then(() => {
-// //     console.log("1");
-// //   })
-// //   .then(() => {
-// //     console.log("2");
-// //   });
+Promise.resolve()
+  .then(() => {
+    console.log("1");
+  })
+  .then(() => {
+    console.log("2");
+  });
 
-// // Promise.resolve().then(() => {
-// //   console.log("3");
-// // });
+Promise.resolve().then(() => {
+  console.log("3");
+});
 
-// // console.log("4");
+console.log("4");
 
-// // console.log("Start");
+console.log("Start");
 
-// // setTimeout(() => {
-// //   console.log("Timeout 1");
-// // }, 0);
+setTimeout(() => {
+  console.log("Timeout 1");
+}, 0);
 
-// // setTimeout(() => {
-// //   console.log("Timeout 2");
-// // }, 0);
+setTimeout(() => {
+  console.log("Timeout 2");
+}, 0);
 
-// // Promise.resolve().then(() => {
-// //   console.log("Promise 1");
-// // });
+Promise.resolve().then(() => {
+  console.log("Promise 1");
+});
 
-// // console.log("End");
+console.log("End");
 
-// console.log("1");
+console.log("1");
 
-// setTimeout(() => {
-//   console.log("2");
+setTimeout(() => {
+  console.log("2");
 
-//   Promise.resolve().then(() => {
-//     console.log("3");
-//   });
-// }, 0);
+  Promise.resolve().then(() => {
+    console.log("3");
+  });
+}, 0);
 
-// Promise.resolve().then(() => {
-//   console.log("4");
+Promise.resolve().then(() => {
+  console.log("4");
 
-//   setTimeout(() => {
-//     console.log("5");
-//   }, 0);
-// });
+  setTimeout(() => {
+    console.log("5");
+  }, 0);
+});
 
-// console.log("6");
+console.log("6");
 
-// // qustion 3
+// qustion 3
 
-// console.log("1");
+console.log("1");
 
-// setTimeout(() => console.log("2"), 0);
+setTimeout(() => console.log("2"), 0);
 
-// Promise.resolve().then(() => {
-//   console.log("3");
-// });
+Promise.resolve().then(() => {
+  console.log("3");
+});
 
-// Promise.resolve().then(() => {
-//   console.log("4");
-// });
+Promise.resolve().then(() => {
+  console.log("4");
+});
 
-// console.log("5");
+console.log("5");
 
-// // Question 4 (Nested Promise)
+// Question 4 (Nested Promise)
 
-// console.log("Start");
+console.log("Start");
 
-// Promise.resolve().then(() => {
-//   console.log("P1");
+Promise.resolve().then(() => {
+  console.log("P1");
 
-//   Promise.resolve().then(() => {
-//     console.log("P2");
-//   });
-// });
+  Promise.resolve().then(() => {
+    console.log("P2");
+  });
+});
 
-// setTimeout(() => {
-//   console.log("T1");
-// }, 0);
+setTimeout(() => {
+  console.log("T1");
+}, 0);
 
-// console.log("End");
+console.log("End");
 
-// // Question 5 (Very Important)
+// Question 5 (Very Important)
 
-// console.log("A");
+console.log("A");
 
-// setTimeout(() => {
-//   console.log("B");
+setTimeout(() => {
+  console.log("B");
 
-//   Promise.resolve().then(() => {
-//     console.log("C");
-//   });
-// }, 0);
+  Promise.resolve().then(() => {
+    console.log("C");
+  });
+}, 0);
 
-// Promise.resolve().then(() => {
-//   console.log("D");
-// });
+Promise.resolve().then(() => {
+  console.log("D");
+});
 
-// console.log("E");
+console.log("E");
 
-// // Question 6 (Chain Confusion)
+// Question 6 (Chain Confusion)
 
-// Promise.resolve()
-//   .then(() => {
-//     console.log("1");
-//   })
-//   .then(() => {
-//     console.log("2");
-//   });
+Promise.resolve()
+  .then(() => {
+    console.log("1");
+  })
+  .then(() => {
+    console.log("2");
+  });
 
-// Promise.resolve().then(() => {
-//   console.log("3");
-// });
+Promise.resolve().then(() => {
+  console.log("3");
+});
 
-// console.log("4");
+console.log("4");
 
-// // Question 7 (Mixed Queue)
+// Question 7 (Mixed Queue)
 
-// console.log("Start");
+console.log("Start");
 
-// setTimeout(() => {
-//   console.log("Timeout 1");
-// }, 0);
+setTimeout(() => {
+  console.log("Timeout 1");
+}, 0);
 
-// setTimeout(() => {
-//   console.log("Timeout 2");
-// }, 0);
+setTimeout(() => {
+  console.log("Timeout 2");
+}, 0);
 
-// Promise.resolve().then(() => {
-//   console.log("Promise 1");
-// });
+Promise.resolve().then(() => {
+  console.log("Promise 1");
+});
 
-// console.log("End");
+console.log("End");
 
-// // Question 8 (Hard)
+// Question 8 (Hard)
 
-// console.log("1");
+console.log("1");
 
-// setTimeout(() => {
-//   console.log("2");
+setTimeout(() => {
+  console.log("2");
 
-//   Promise.resolve().then(() => {
-//     console.log("3");
-//   });
-// }, 0);
+  Promise.resolve().then(() => {
+    console.log("3");
+  });
+}, 0);
 
-// Promise.resolve().then(() => {
-//   console.log("4");
+Promise.resolve().then(() => {
+  console.log("4");
 
-//   setTimeout(() => {
-//     console.log("5");
-//   }, 0);
-// });
+  setTimeout(() => {
+    console.log("5");
+  }, 0);
+});
 
-// console.log("6");
+console.log("6");
 
 // Promish ALL and Promish setteled
 
